34ac71f6a8e781e9d6e0a530163f29ad4b56c757
krasnovss91 <krasnovstanislav91@gmail.com>
2020-11-07 13:36:40 +0400

    Service

--- src/main/java/com/boraji/tutorial/spring/controller/MyContoller.java
+++ src/main/java/com/boraji/tutorial/spring/controller/MyContoller.java
@@ -2,12 +2,14 @@ package com.boraji.tutorial.spring.controller;
 
 import java.security.Principal;
 
+import com.boraji.tutorial.spring.service.UserDetailsServiceImp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyContoller {
+  private UserDetailsServiceImp userDetailsService;
 
   @GetMapping("/")
   public String index(Model model, Principal principal) {
@@ -16,13 +18,13 @@ public class MyContoller {
   }
   @GetMapping("/list-of-users")
   public String showUsers(Model model) {
-    model.addAttribute("users", userService.getAllUsers());
+    model.addAttribute("users", .getAllUsers());
     return "list-of-users";
   }
 
   @GetMapping("/")
   public String showAll(Model model) {
-    model.addAttribute("users", userService.getAllUsers());
+    model.addAttribute("users", userDetailsService.getAllUsers());
     return "navigation";
   }
 
@@ -30,9 +32,9 @@ public class MyContoller {
   @PostMapping("showUserForm/add")
   public String addUser(@ModelAttribute User user) {
     if (user.getId() == 0) {
-      userService.saveUser(user);
+      userDetailsService.saveUser(user);
     } else {
-      userService.editUser(user);
+      userDetailsService.editUser(user);
     }
     return "redirect:/showUserForm";
   }
@@ -40,15 +42,15 @@ public class MyContoller {
   @GetMapping("/showUserForm")
   public String showUserForm(Model model) {
     model.addAttribute("user", new User());
-    model.addAttribute("listUsers", userService.getAllUsers());
+    model.addAttribute("listUsers", userDetailsService.getAllUsers());
     return "user-form";
   }
 
   @GetMapping("showUserForm/edit/{id}")
   public String editUser(@PathVariable long id, Model model) {
 
-    model.addAttribute("user", userService.getUserById(id));
-    model.addAttribute("listUsers", userService.getAllUsers());
+    model.addAttribute("user", userDetailsService.getUserById(id));
+    model.addAttribute("listUsers", userDetailsService.getAllUsers());
 
 
     return "edit-user";
@@ -56,21 +58,21 @@ public class MyContoller {
 
   @PostMapping("showUserForm/edit/showUserForm/edit")
   public String editUser(@ModelAttribute("editUser") User user) {
-    userService.editUser(user);
+    userDetailsService.editUser(user);
     return "redirect:/showUserForm";
   }
 
 
   @GetMapping("showUserForm/delete/{id}")
   public String deleteUser(@PathVariable("id") long id) {
-    userService.deleteUser(id);
+    userDetailsService.deleteUser(id);
     return "redirect:/showUserForm";
 
   }
 
   @GetMapping("/{id}")
   public String userData(@PathVariable long id, Model model) {
-    model.addAttribute("user", this.userService.getUserById(id));
+    model.addAttribute("user", this.userDetailsService.getUserById(id));
     return "list-of-users";
   }
 

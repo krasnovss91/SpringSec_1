6fc728aa57944124ed57d927cef2c82152228176
krasnovss91 <krasnovstanislav91@gmail.com>
2020-11-22 17:35:21 +0400

    Controller_12

--- src/main/java/spring_sec_1/controller/MyContoller.java
+++ src/main/java/spring_sec_1/controller/MyContoller.java
@@ -33,7 +33,7 @@ public class MyContoller {
   }
 
 
-  @PostMapping("showUserForm/add")
+  @PostMapping("admin/add")
   public String addUser(@ModelAttribute User user) {
     if (userDetailsService.findUserByName(user.getUsername())){
       userDetailsService.saveUser(user);
@@ -43,14 +43,14 @@ public class MyContoller {
     return "redirect:/showUserForm";
   }
 
-  @GetMapping("/showUserForm")
+  @GetMapping("/admin")
   public String showUserForm(Model model) {
     model.addAttribute("user", new User());
     model.addAttribute("listUsers", userDetailsService.getAllUsers());
     return "admin";
   }
 
-  @GetMapping("showUserForm/edit/{id}")
+  @GetMapping("admin/edit/{id}")
   public String editUser(@PathVariable long id, Model model) {
 
     model.addAttribute("user", userDetailsService.getUserById(id));
@@ -59,14 +59,14 @@ public class MyContoller {
     return "admin";
   }
 
-  @PostMapping("showUserForm/edit/showUserForm/edit")
+  @PostMapping("admin/edit/showUserForm/edit")
   public String editUser(@ModelAttribute("editUser") User user) {
     userDetailsService.editUser(user);
     return "redirect:/showUserForm";
   }
 
 
-  @GetMapping("showUserForm/delete/{id}")
+  @GetMapping("admin/delete/{id}")
   public String deleteUser(@PathVariable("id") long id) {
     userDetailsService.deleteUser(id);
     return "redirect:/showUserForm";
